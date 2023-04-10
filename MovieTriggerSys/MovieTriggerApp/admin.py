b1b67e78bf33b480e44d92efcdb0064e5662cdb4
unknown <76260193+fatmaboodai@users.noreply.github.com>
2023-04-10 23:27:25 +0300

    flex

--- MovieTriggerSys/MovieTriggerApp/admin.py
+++ MovieTriggerSys/MovieTriggerApp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from . import models
+from django.urls import reverse
+from django.db.models import Count
+from django.utils.http import  urlencode
+from django.utils.html import format_html
 # Register your models here.
