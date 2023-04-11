8e509fa4ca094cbfab113ea9dafe86a6d36300a0
unknown <76260193+fatmaboodai@users.noreply.github.com>
2023-04-11 21:56:49 +0300

    form is done (search fields also) (inline children not doen)

--- MovieTriggerSys/MovieTriggerApp/admin.py
+++ MovieTriggerSys/MovieTriggerApp/admin.py
@@ -8,10 +8,25 @@ from django.utils.html import format_html
 
 
 
+
+#  this is the inline children code its not working
+# # we can write a trigger while creating a movie 
+# # add a children inline 
+# class TriggerInline(admin.StackedInline):
+#     # overriding thew model i want to make it inline
+#     model = models.Trigger
+#     extra = 0
+
+
+# MOVIE MODEL
 @admin.register(models.Movie)
 class MovieAdmin(admin.ModelAdmin):
+    # form fields
+    fields=[("title","description"),('age_rating','trigger','genre')]
+    # search field so it can be searchable 
+    search_fields=['title']
+    
     # display fields 
-    # fields=[("description","")]
     fields = ['trigger']
     list_display=['MID','title','description','age_rating','TopMovies', 'trigger','trigger_name']
     list_editable=['age_rating']
@@ -20,6 +35,17 @@ class MovieAdmin(admin.ModelAdmin):
     #list_select_related=['trigger']
     # ordering = ['Top5']
 
+    # to make the genre and the trigger searcheable in the form
+    autocomplete_fields=['trigger','genre']
+
+
+# this is the inline children code its not working
+    # Refernce the inline class 
+    # inlines = [TriggerInline,]
+
+
+
+#   COMPUTED COLUMN
     @admin.display(ordering='TopMovies' ,description="Is it in the top movies ?",boolean=True)
     def TopMovies(self,Movie:models.Movie):
         if Movie.title == "Viridiana":
@@ -27,11 +53,11 @@ class MovieAdmin(admin.ModelAdmin):
         if Movie.title == "Hot Rod":
             return True  
         return False
-       
+    #    OVEERINDING BASE QUERYSET
     def get_queryset(self, request):
         return super().get_queryset(request).annotate(TopMovies=Value(True))
     
-
+# RELATED CLOUMN
     @admin.display(ordering='trigger__name')
     def trigger_name(self,movie:models.Movie):
         return movie.trigger.name
@@ -40,43 +66,84 @@ class MovieAdmin(admin.ModelAdmin):
 
 
 
+
+
+
+# VIEWER MODEL
 @admin.register(models.Viewer)
 class ViewerAdmin(admin.ModelAdmin):
+    # FORM FIELDS
+    fields=[("email","password")]
     # display fields 
     list_display=['VID','email','password']
     list_per_page=10
     list_filter=['email']
 
 
+
+#this is the inline children code its not working
+# we can create a movie while creating a list
+# add a children inline 
+# class MovieInline(admin.StackedInline):
+#     # overriding thew model i want to make it inline
+#     model = models.Movie
+#     extra = 0
+#     # what is the name of the fk 
+#     fk_name = model.trigger
+
+
+
+# LIST MODEL
 @admin.register(models.List)
 class ListAdmin(admin.ModelAdmin):
+    # form fields
+    fields=[("name","description"),('movie','viewer')]
     # display fields 
     list_display=['LID','name','description']
     list_per_page=10
     list_filter=['name']
 
 
+    #  this is the inline children code its not working
+    # refrence the inline 
+    # inlines = [MovieInline,]
 
+
+# TRIGGER MODEL
 @admin.register(models.Trigger)
 class TriggerAdmin(admin.ModelAdmin):
+    # form fields
+    fields=[("name","description")]
     # display fields 
     list_display=['name','description']
     list_per_page=10
     list_filter=['name']
+    # search field so it can be used in the autocomplete field
+    search_fields=['trigger']
 
 
+# GENRE MODEL
 @admin.register(models.Genre)
 class GenreAdmin(admin.ModelAdmin):
+    # form fields
+    fields=[("genre")]
     # display fields 
     list_display=['id','genre']
     list_editable=['genre']
     list_per_page=10
     list_filter=['genre']
+    # search field so it can be used in the autocomplete field
+    search_fields=['genre']
 
 
 
+
+
+# REVIEW MODEL
 @admin.register(models.Review)
 class ReviewAdmin(admin.ModelAdmin):
+    # form fields
+    fields=[("description"),('age_rating','movie','viewer')]
     # display fields 
     list_display=['RID','description','rating']
     list_per_page=10
--- MovieTriggerSys/MovieTriggerApp/models.py
+++ MovieTriggerSys/MovieTriggerApp/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.forms import ValidationError
-from django.core.validators import MinValueValidator,MaxValueValidator
+from django.core.validators import MinValueValidator,MaxValueValidator,MinLengthValidator,MaxLengthValidator
 
 
 # Create your models here.
@@ -22,17 +22,22 @@ class Movie(models.Model):
 
     MID = models.CharField(max_length = 255, primary_key=True)
     title = models.CharField(max_length = 255)
-    description = models.TextField()
+    #  blank is for the admin interface validation that the description is optional (form validation)
+    description = models.TextField(blank=True)
     age_rating = models.CharField(max_length  = 4, choices = AGE_RATINGS, default = RATED_G)
     genre = models.ForeignKey('Genre', on_delete=models.PROTECT)
-    trigger = models.ForeignKey('Trigger', on_delete=models.PROTECT)
+    # a trigger is optional in a movie (form validation)
+    trigger = models.ForeignKey('Trigger', on_delete=models.PROTECT,blank=True)
     review = models.ForeignKey('Review', on_delete=models.PROTECT)
+    def __str__(self):
+        return self.title
 
 
 
 class Trigger(models.Model):
     name = models.CharField(max_length = 255)
-    description = models.TextField()
+    #  blank is for the admin interface validation that the description is optional (form validation)
+    description = models.TextField(blank=True)
 
     def __str__(self):
         return self.name
@@ -56,23 +61,32 @@ class Genre(models.Model):
         (ROMANCE, 'Romance')
     ]
     genre = models.CharField(max_length = 255, choices = GENRE_CHOICES, default = DRAMA)
+    def __str__(self):
+        return self.genre
 
 class Review(models.Model):
     RID = models.CharField(max_length = 255, primary_key=True)
     Viewer = models.ForeignKey('Viewer', on_delete=models.PROTECT)
-    description = models.TextField()
-    rating = models.IntegerField(validators = (MinValueValidator(1),MaxValueValidator(5)))
+    #  blank is for the admin interface validation that the description is optional (form validation)
+    description = models.TextField(blank=True)
+    # form validations for the ratings
+    rating = models.IntegerField(validators = [MinValueValidator(1),MaxValueValidator(5)])
+    
 
 
 
 class Viewer(models.Model):
     VID = models.CharField(max_length = 255, primary_key=True)
     email = models.EmailField(unique = True)
-    password = models.CharField(max_length=255)
+    # Validators for the viewer's password
+    password = models.CharField(max_length=255 , validators=[MinLengthValidator(8),MaxLengthValidator(20)])
+    def __str__(self):
+        return self.email
 
 class List(models.Model):
     LID = models.CharField(max_length = 255, primary_key=True)
     name = models.CharField(max_length = 255)
-    description = models.TextField()
+     #  blank is for the admin interface validation that the description is optional (form validation)
+    description = models.TextField(blank=True)
     movie = models.ForeignKey('Movie', on_delete=models.PROTECT)
     viewer = models.ForeignKey('Viewer', on_delete=models.PROTECT)
